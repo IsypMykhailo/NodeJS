4abcb0e033574eaac56341afe26900453f708399
IsypMykhailo <amazonkanameforme@gmail.com>
2022-04-23 19:13:17 +0200

    react

---
+++ 07_jsFrameworks/my-react/src/components/MyReactive.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+class MyReactive extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            myName: "Mykhailo"
+        }
+    }
+
+    onInputChange(ev){
+        const state = this.state
+        state.myName = ev.target.value;
+        this.setState(state);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Привет, {this.state.myName}</h1>
+                <input onChange={this.onInputChange.bind(this)}></input>
+            </div>
+        )
+
+    }
+}
+
+export default MyReactive
