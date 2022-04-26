81b135c934b86fe28fe5f7a2c33ee7c46f92b118
IsypMykhailo <amazonkanameforme@gmail.com>
2022-04-26 17:51:38 +0200

    reactRouting

--- 07_jsFrameworks/my-react/src/App.js
+++ 07_jsFrameworks/my-react/src/App.js
@@ -1,13 +1,32 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import MyContactForm from "./components/MyContactForm";
-import MyReactive from "./components/MyReactive";
-import MyArray from "./components/MyArray";
+// eslint-disable-next-line
+import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
+//import MyContactForm from "./components/MyContactForm";
+//import MyReactive from "./components/MyReactive";
+//import MyArray from "./components/MyArray";
+import Header from "./layouts/Header";
+import Footer from "./layouts/Footer";
+import PageHome from "./pages/Home";
+import PageAbout from "./pages/About";
+import PageContact from "./pages/Contact";
+import PageError404 from "./pages/Error404";
 
 function App() {
   return (
     <div className="App">
-        <MyArray></MyArray>
+        <Header></Header>
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<PageHome />} />
+            <Route path="about" element={<PageAbout />} />
+            <Route path="contact" element={<PageContact />} />
+            <Route path="*" element={<PageError404 />} />
+        </Routes>
+        </BrowserRouter>
+        <Footer></Footer>
     </div>
   );
 }
---
+++ 07_jsFrameworks/my-react/src/layouts/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Footer extends React.Component {
+    render(){
+        return(
+            <footer>&copy; 2022 ITSTEP</footer>
+        )
+    }
+};
+export default Footer;
