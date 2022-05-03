0be197f2738b5110032baac086c77e9caffd7021
IsypMykhailo <amazonkanameforme@gmail.com>
2022-05-03 19:03:06 +0200

    BackEnd Media Helper

--- 03_express/routes/helpers/media-converter.js
+++ 03_express/routes/helpers/media-converter.js
@@ -1,8 +1,9 @@
-let express = require('express');
-let router = express.Router();
+let express = require('express')
+let router = express.Router()
 
 let c = require('../../helpers/media/media-converter')
 
 router.post('/avatar', c.avatar)
-// router.post('/', c.post)
+// router.post('/', p.post)
+
 module.exports = router;
