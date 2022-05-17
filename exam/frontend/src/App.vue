af300ab484ae636316fbcde9bde2fd4889a0c64c
IsypMykhailo <amazonkanameforme@gmail.com>
2022-05-17 17:11:55 +0200

    exam

---
+++ exam/frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Translate></Translate>
+</template>
+
+<script>
+    import Translate from './components/Translate.vue'
+
+export default {
+  name: 'App',
+  components: {
+      Translate
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
