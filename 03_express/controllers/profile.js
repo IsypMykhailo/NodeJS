3eadd972a12a4087369caf4bf3884f578f552718
IsypMykhailo <amazonkanameforme@gmail.com>
2022-05-19 18:55:14 +0200

    moveFile

---
+++ 03_express/controllers/profile.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const path = require('path')
+const users = require('../models/user')
+
+exports.updateAvatar = function (request,response) {
+    if(!request.user) {
+        console.log('No User')
+        return response.status(403)
+            .json({ message: 'Not authorized' })
+    }
+
+    let upFile = request.body.filename
+
+    let fromFile = path.join(__dirname, '../../uploads/', upFile)
+    let toFile = path.join(__dirname, '../../public/storage/avatars/', upFile)
+
+    fs.rename(fromFile,toFile,function(err){
+        if(err){
+            return response.status(422)
+                .json({message:'Cannot rename'})
+        }
+        try {
+            users.updateOne({_id: request.user._id}, [
+                {$set: {avatar: upFile.name, updated_at: Date.now()}},
+                {$unset: []}
+            ])
+            return response.status(204)
+                .json({avatarUrl: upFile})
+        } catch (e) {
+            response.status(422).json(e)
+        }
+    })
+}
